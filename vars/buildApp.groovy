f9877e1b5eaee5344286bba6ddf98e9bf792fa0b
redhatHameed <41960152+redhatHameed@users.noreply.github.com>
2018-12-03 15:51:54 -0500

    Update buildApp.groovy

--- vars/buildApp.groovy
+++ vars/buildApp.groovy
@@ -1,7 +1,8 @@
 #!/usr/bin/groovy
 
 def call(project, app){
-    projectSet(project)
+    #projectSet(project)
+    sh "pwd"
     sh "oc new-build -n ${project} --binary --name=${app} -l app=${app} || echo 'Build exists'"
     sh "oc start-build ${app} -n ${project} --from-dir=. --follow"
     deployApp(project, app, 'latest')
